b8c5030e756e70651bf08cac1cde4bcbb80dcf60
wilson360-labs <dg242181@gmail.com>
2026-02-11 15:11:48 -0600

    Last Sync: 2026-02-11 15:11 (Mobile)

--- script.js
+++ script.js
@@ -14,5 +14,5 @@ window.addEventListener('load', () => {
     splashScreen.style.transition = 'opacity 8s ease-out';
     setTimeout(() => {
         splashScreen.style.display = 'none';
-    }, 4000); // Asegúrate de que el splash screen se oculte después de la animación
+    }, 6000); // Asegúrate de que el splash screen se oculte después de la animación
 });
